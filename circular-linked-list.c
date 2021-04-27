3ef1b697435dfc9aa4c4243ac8e748935dde0981
seyeonJeong <wjdtpdus828@naver.com>
2021-04-27 20:01:18 +0900

    commit 3

--- circular-linked-list.c
+++ circular-linked-list.c
@@ -153,16 +153,16 @@ void printList(listNode* h) {
 
 	printf("\n---PRINT\n");
 
-	if(h == NULL) {
+	if (h == NULL) {//헤더노드의 값이 NULL이라면
 		printf("Nothing to print....\n");
 		return;
 	}
 
-	p = h->rlink;
+	p = h->rlink; // p를 헤더노드의 rlink가 가리키는 값으로 변경
 
-	while(p != NULL && p != h) {
+	while (p != NULL && p != h) {//p가 NULL값이고 p가 헤더노드가 일때까지
 		printf("[ [%d]=%d ] ", i, p->key);
-		p = p->rlink;
+		p = p->rlink; // p의 값을 p의 rlink가 가리키는 값으로 변경
 		i++;
 	}
 	printf("  items = %d\n", i);
@@ -174,10 +174,10 @@ void printList(listNode* h) {
 	printf("head node: [llink]=%p, [head]=%p, [rlink]=%p\n", h->llink, h, h->rlink);
 
 	i = 0;
-	p = h->rlink;
-	while(p != NULL && p != h) {
+	p = h->rlink; // 헤더노드의 rlink가 가리키는 값을 p에 대입
+	while (p != NULL && p != h) {
 		printf("[ [%d]=%d ] [llink]=%p, [node]=%p, [rlink]=%p\n", i, p->key, p->llink, p, p->rlink);
-		p = p->rlink;
+		p = p->rlink; // p의 가리키는 노드를 p의 rlink가 가리키는 노드로 변경
 		i++;
 	}
 
@@ -189,6 +189,30 @@ void printList(listNode* h) {
  * list에 key에 대한 노드하나를 추가
  */
 int insertLast(listNode* h, int key) {
+
+	listNode* n1 = (listNode*)malloc(sizeof(listNode)); // 동적할당을 이용하여 n1 노드를 생성
+	listNode* cur = h->rlink; // 탐색할 때 사용하는 노드를 가리키는 포인터 cur을 헤더노드가 가리키는 값으로 설정
+
+	n1->key = key;//n1의 key값에 매개변수로 가져온 key값을 저장
+
+	if (h->rlink == h)//첫번째 헤더노드가 가리키는 값이 헤더노드라면, 즉 노드가 없는 상태라면
+	{
+		n1->llink = h; // n1의 llink가 h를 가리키게 하고
+		n1->rlink = h;//n1의 rlink가 h를 가리키게함 (n1이 첫번째 노드라면 원형리스트이므로 rlink가 헤더노드를 가리켜야함)
+		h->rlink = n1;//h의 rlink가 n1을 가리키게함
+		h->llink = n1;//h의 llink도 n1을 가리킴(n1이 첫번째 노드이자 마지막 노드라면 헤더노드의 llink는 마지막 노드를 가리켜야함)
+		return 0;
+	}
+	while (cur->rlink != h) // cur의 rlink값이 헤더노드가 아닐때 까지 반복
+	{
+		cur = cur->rlink; // cur을 cur의 rlink가 가리키는 노드로 변경
+	}
+
+	//삽입은 이중연결리스트와 비슷하지만 n1의 rlink가 가리키는 값을 헤더노드로 설정해주고 헤더노드의 llink가 가리키는 값을 n1으로 설정해주면서 원형리스트의 특징을 갖게 하는 작업.
+	n1->llink = cur; // n1의 llink가 가리키는 노드는 cur이 됨.
+	n1->rlink = h; //n1의 rlink가 가리키는 노드는 헤더노드가 됨.
+	cur->rlink = n1; //cur의 rlink가 가리키는 노드는 n1이 됨.
+	h->llink = n1; //h의 llink가 가리키는 노드는 n1이 됨.
 
 	return 1;
 }
